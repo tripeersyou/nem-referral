e43210f6a7be2f4932c8bac0c5ea46a8a83c9ebc
Francis Oliver Avancena <francis.avancena@obf.ateneo.edu>
2019-04-25 01:24:56 +0000

    Dashboard finish

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,43 @@
 const nem = require('nem-sdk').default;
 let address = document.querySelector('#address').value;
 document.addEventListener('DOMContentLoaded', function(){
-   const connect = (connector) => {
-        return connector.connect().then(() =>{
-            nem.com.websockets.subscribe.account.data(connector, function(res) {
-                console.log(res);
-            });
-            nem.com.websockets.subscribe.account.transactions.recent(connector, function(res){
-                console.log(res);
-            });
-            nem.com.websockets.subscribe.account.transactions.unconfirmed(connector, function(res) {
-                console.log(res);
-            });
-            nem.com.websockets.subscribe.account.transactions.confirmed(connector, function(res) {
-                console.log(res);
-            });
-        });
-       
-   }
-   
-   const start = () =>{
-       var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.websocketPort);
-       var account_address = nem.model.address.clean(address);
-       var connector = nem.com.websockets.connector.create(endpoint, account_address);
-       connect(connector);
-   }
-   
-   start();
+    $.getJSON(`https://cors.io/?http://23.228.67.85:7890/account/get?address=${address}`, function(res){
+       console.log(res);
+       $('#accountDetails').html(`<div class="well">
+        <b>Balance</b>: ${res.account.balance}
+        <br>
+        <b>Importance</b>: ${res.account.importance}
+        <br>
+        <b>Harvested Blocks</b>: ${res.account.harvestedBlocks}
+       </div>`);
+    });
+    $.getJSON(`https://cors.io/?http://23.228.67.85:7890/account/transfers/outgoing?address=${address}`, function(res){
+        console.log(res);
+        let data = '';
+        for(let tx of res.data){
+            
+            if (tx.transaction.amount != undefined) {
+                data += `<div class="well">`;
+                data += `<b>Amount: </b>${tx.transaction.amount}<br>`
+                data += `<b>Fee: </b>${tx.transaction.fee}<br>`
+                data += `<b>Transaction Hash: </b>${tx.meta.hash.data}<br>`
+                data += `</div>`
+            }
+        }
+        $('#outgoingTransfers').html(data);
+    });
+    $.getJSON(`https://cors.io/?http://23.228.67.85:7890/account/transfers/incoming?address=${address}`, function(res){
+        console.log(res);
+        let data = '';
+        for(let tx of res.data){
+            data += `<div class="well">`;
+            data += `<b>Amount: </b>${tx.transaction.amount}<br>`
+            data += `<b>Fee: </b>${tx.transaction.fee}<br>`
+            data += `<b>Transaction Hash: </b>${tx.meta.hash.data}<br>`
+            data += `</div>`
+        }
+        $('#incomingTransfers').html(data);
+    });
+    
 });
 
